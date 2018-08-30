be5bdba195714557511ebbabf26964d2b7bfd7ce
JISUN <JISUN@192.168.0.104>
2018-08-30 16:31:19 +0900

    diary setter/getter

--- semi-project/src/org/kosta/model/DiaryVO.java
+++ semi-project/src/org/kosta/model/DiaryVO.java
@@ -1,5 +1,40 @@
 package org.kosta.model;
 
 public class DiaryVO extends BoardVO{
-
+	private String dno;
+	private int state;
+	private boolean isPublic;
+	public DiaryVO(String dno,String title, String content, String regDate, MemberVO vo, int state, boolean isPublic) {
+		super(title, content, regDate, vo);
+		this.dno = dno;
+		this.state = state;
+		this.isPublic = isPublic;
+	}
+	public DiaryVO() {
+		super();
+	}
+	public DiaryVO(String title, String content, MemberVO vo,int state,boolean isPublic) {
+		super(title, content, vo);
+		this.state=state;
+		this.isPublic=isPublic;
+	}
+	public String getDno() {
+		return dno;
+	}
+	public void setDno(String dno) {
+		this.dno = dno;
+	}
+	public int getState() {
+		return state;
+	}
+	public void setState(int state) {
+		this.state = state;
+	}
+	public boolean isPublic() {
+		return isPublic;
+	}
+	public void setPublic(boolean isPublic) {
+		this.isPublic = isPublic;
+	}
+	
 }
