4b82fc1a3fb7cb27948fc480057fdd1892d09842
JISUN <JISUN@192.168.0.104>
2018-08-31 17:30:27 +0900

    diary 에서 state 제거

--- semi-project/src/model/DiaryVO.java
+++ semi-project/src/model/DiaryVO.java
@@ -2,20 +2,17 @@ package model;
 
 public class DiaryVO extends BoardVO{
 	private String dno;
-	private int state;
 	private boolean isPublic;
-	public DiaryVO(String dno,String title, String content, String regDate, MemberVO vo, int state, boolean isPublic) {
+	public DiaryVO(String dno,String title, String content, String regDate, MemberVO vo, boolean isPublic) {
 		super(title, content, regDate, vo);
 		this.dno = dno;
-		this.state = state;
 		this.isPublic = isPublic;
 	}
 	public DiaryVO() {
 		super();
 	}
-	public DiaryVO(String title, String content, MemberVO vo,int state,boolean isPublic) {
+	public DiaryVO(String title, String content, MemberVO vo,boolean isPublic) {
 		super(title, content, vo);
-		this.state=state;
 		this.isPublic=isPublic;
 	}
 	public String getDno() {
@@ -23,12 +20,6 @@ public class DiaryVO extends BoardVO{
 	}
 	public void setDno(String dno) {
 		this.dno = dno;
-	}
-	public int getState() {
-		return state;
-	}
-	public void setState(int state) {
-		this.state = state;
 	}
 	public boolean isPublic() {
 		return isPublic;
