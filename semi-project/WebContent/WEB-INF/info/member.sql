9d16c9dc15ed86bc3dd2d5192135553d160282d5
moonblin <mjw0613@gmail.com>
2018-09-05 14:27:15 +0900

    준위: id중복확인(탈퇴적용)

--- semi-project/WebContent/WEB-INF/info/member.sql
+++ semi-project/WebContent/WEB-INF/info/member.sql
@@ -14,7 +14,7 @@ values ('hsjhsj','1234','테스트2','hsj@네이버','20180903',1)
 
 select * from GREEN_MEMBER
 
-update green_member set state=0 where id='66';
+update green_member set state=0 where id='java';
 
 select password from green_member where id='1'
 
